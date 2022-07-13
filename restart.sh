b9b68f3a81782a8491430d835228576aff193a82
bjarkiarmanns <bjarki@grammatek.com>
2022-07-13 15:48:10 +0000

    Removed redundant commands in dockerfile and bash file.

--- restart.sh
+++ restart.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo docker stop action-server2
-sudo docker rm action-server2
+docker stop action-server2
+docker rm action-server2
 
-sudo docker build -f dockerfile -t sdifi_rasa_3 .
-sudo docker build -f sdk-dockerfile -t sdk_image .
+docker build -f dockerfile -t sdifi_rasa_3 .
+docker build -f sdk-dockerfile -t sdk_image .
 
-sudo docker run -d -v $(pwd)/actions:/app/actions --net my-project3 --name action-server2 sdk_image:latest
-sudo docker run -it -v $(pwd):/app -p 5005:5005 --net my-project3 sdifi_rasa_3:latest train
-sudo docker run -it -v $(pwd):/app -p 5005:5005 --net my-project3 sdifi_rasa_3:latest shell
+docker run -d -v $(pwd)/actions:/app/actions --net my-project3 --name action-server2 sdk_image:latest
+docker run -it -v $(pwd):/app -p 5005:5005 --net my-project3 sdifi_rasa_3:latest train
+docker run -it -v $(pwd):/app -p 5005:5005 --net my-project3 sdifi_rasa_3:latest shell
