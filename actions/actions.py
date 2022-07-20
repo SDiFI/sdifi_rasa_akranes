fccd9cc35571174b240f7a9c0c6b5ce9aa08f57a
RagnarP5 <raggipals@gmail.com>
2022-07-20 17:28:48 +0000

    Added template actions for getting info, phone and email

--- actions/actions.py
+++ actions/actions.py
@@ -38,14 +38,43 @@ class ActionGetContactInfoFromContact(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         contact = tracker.get_slot('contact')
+        dispatcher.utter_message(text="Ég er inni í ActionGetContactInfoFromContact.run()")
 
-        g = Graph()
-        g.parse('offices_staff.rdf')
+        # g = Graph()
+        # g.parse('offices_staff.rdf')
         
-        query = sparql_queries.get_contact_info_query(contact)
-        result = g.query(query)
+        # query = sparql_queries.get_contact_info_query(contact)
+        # result = g.query(query)
         
-        for r in result:
-            dispatcher.utter_message(text=r['name'] + " er með netfangið: " + r['email'])
+        # for r in result:
+        #     dispatcher.utter_message(text=r['name'] + " er með netfangið: " + r['email'])
         
         return [Restarted()]
+
+
+class ActionGetContactNumberFromContact(Action):
+    def name(self) -> Text:
+        return "action_get_contact_number"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        contact = tracker.get_slot('contact')
+        dispatcher.utter_message(text="Ég er inni í ActionGetContactNumberFromContact.run()")
+        return []
+
+
+class ActionGetContactEmailFromContact(Action):
+    def name(self) -> Text:
+        return "action_get_contact_email"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        contact = tracker.get_slot('contact')
+        dispatcher.utter_message(text="Ég er inni í ActionGetContactEmailFromContact.run()")
+        return []
+
+
