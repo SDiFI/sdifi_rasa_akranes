77a7d5a9980973ef3438b4f556d2a987d55b6f3a
bjarkiarmanns <bjarki@grammatek.com>
2023-04-25 14:58:57 +0000

    Amend incorrect commit.

--- actions/actions.py
+++ actions/actions.py
@@ -302,14 +302,8 @@ class ActionGetOperator(Action):
     ) -> List[Dict[Text, Any]]:
 
         res = info_api.get_office_contact_info()
-        if res:
-            r = res[0]
-            if r.phone and r.email:
-                dispatcher.utter_message(response="utter_operator", phone=r.phone, email=r.email)
-            else:
-                dispatcher.utter_message(response="utter_operator_no_info")
-        else:
-            dispatcher.utter_message(response="utter_operator_no_info")
+        r = res[0]
+        dispatcher.utter_message(response="utter_operator", phone=r.phone, email=r.email)
 
         return []
 
